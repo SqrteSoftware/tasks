51b651c420d980e8e36aed14554e26fbee0e047c
Andy Nemzek <virtualhigh5@gmail.com>
2021-05-07 23:40:37 -0500

    Use new license key generator

--- src/utils/license.js
+++ src/utils/license.js
@@ -1,5 +1,4 @@
-import uuidv4 from 'uuid/v4'
-import {createEncodedKeypair} from './crypto';
+import {generateLicenseKey, createEncodedKeypair} from './crypto';
 import {loadUrlQueryParams} from '../utils'
 import {createPaymentSession, createLicenseKey} from '../actions/licenseActions';
 
@@ -7,7 +6,7 @@ export function handleNewSession(store) {
     let queryParams = loadUrlQueryParams();
     if (queryParams.session) {
         store.dispatch(createPaymentSession(queryParams.session));
-        let license = uuidv4().toUpperCase();
+        let license = generateLicenseKey();
         createEncodedKeypair(license).then(keyInfo => {
             console.log("LICENSE:",license);
             console.log("KEY GENERATED:",keyInfo);
