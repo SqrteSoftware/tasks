5531a17421cc0483720e3ee92f19e84c02f3a6ac
Andy Nemzek <virtualhigh5@gmail.com>
2021-07-20 00:31:55 -0500

    Initial stab at storing keypack

--- src/utils/license.js
+++ src/utils/license.js
@@ -4,12 +4,26 @@ import {createPaymentSession, createLicenseKey} from '../actions/licenseActions'
 
 export function handleNewRegistration(store) {
     let queryParams = loadUrlQueryParams();
-    if (queryParams.session) {
-        store.dispatch(createPaymentSession(queryParams.session));
+    if (queryParams.session_id) {
+        store.dispatch(createPaymentSession(queryParams.session_id));
         let license = generateLicenseKey();
         createKeypack(license).then(keyInfo => {
             console.log("LICENSE:",license);
             console.log("KEY GENERATED:",keyInfo);
+
+            fetch('https://edk22w6pt5.execute-api.us-east-1.amazonaws.com/staging/users', {
+                method: 'PUT',
+                // mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'omit',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': queryParams.session_id
+                },
+                body: JSON.stringify({Item: {id: '1234',...keyInfo}})
+            }).then(resp => {
+            console.log("RESPONSE:",resp);
+            });
             store.dispatch(createLicenseKey(license));
         });
     }
