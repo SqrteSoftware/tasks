92df373049ce62a30ec7db63a7b768946433091e
Andy Nemzek <virtualhigh5@gmail.com>
2022-08-18 23:17:58 -0500

    Switch current AWS to be prod env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var base_url = 'https://0qog733y06.execute-api.us-east-1.amazonaws.com/production';
 if (process.env.NODE_ENV === 'development') {
-    base_url = 'https://0qog733y06.execute-api.us-east-1.amazonaws.com/staging'
+    base_url = 'https://0qog733y06.execute-api.us-east-1.amazonaws.com/production'
 }
 export const BASE_URL = base_url;
 
