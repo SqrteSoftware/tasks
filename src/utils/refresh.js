751ca1751db7e6a6a229649555e724a5393f5bf3
Andy Nemzek <virtualhigh5@gmail.com>
2024-02-15 23:39:32 -0600

    Disable GA

--- src/utils/refresh.js
+++ src/utils/refresh.js
@@ -1,19 +1,36 @@
-let lastLoadedTime = Date.now();
+import { v4 as uuidv4 } from 'uuid';
+
+let lastLoadedTime = Date.now()
 
 export function keepFresh() {
-    window.addEventListener('blur', async () => {
-        // The tab has lost focus. Check to see if it's been more than
-        // a day since refresh. If it has, perform a refresh.
-        let currentTime = Date.now();
-        if (currentTime - lastLoadedTime > (60 * 60 * 24 * 1000)) {
-            try {
-                // Check if we're online - will throw exception if not
-                await fetch(window.location.href, {method: 'HEAD'});
-                // If we're online, refresh the page
-                window.location.reload();
-            } catch {}
+    window.addEventListener('visibilitychange', async () => {
+        if (isOnline()) {
+            let currentTime = Date.now();
+            if (currentTime - lastLoadedTime > (60 * 60 * 24 * 1000)) {
+                // If it's been more than a day since
+                // refresh then perform a refresh.
+                refresh()
+            }
         }
     });
+}
+
+async function isOnline() {
+    try {
+        // Will throw exception if not online
+        await fetch(window.location.href + uuidQuery(), {method: 'HEAD'});
+        return true
+    } catch {
+        return false
+    }
+}
+
+function uuidQuery() {
+    if (! localStorage.getItem('uuid')) {
+        localStorage.setItem('uuid', uuidv4())
+    }
+    let query = '?' + new URLSearchParams({id: localStorage.getItem('uuid')})
+    return query
 }
 
 export function refresh() {
